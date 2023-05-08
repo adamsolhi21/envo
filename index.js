cde5216560f8bcfe165c74b2933b7eed4e7e3fa9
AdamTravers <83338326+adamsolhi21@users.noreply.github.com>
2023-05-08 20:18:39 +0100

    Add files via upload
    updates to experimental designs

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:\Users\AdamTravers\OneDrive - Lovehoney Group\Desktop\Dev\envo\envo-377917-firebase-adminsdk-ohqrz-f0975a7409.json");
+
+admin.initializeApp({
+    
+    credential: admin.credential.cert({
+        "cea1b47ceb00dedeaf4da9a714e3e79f45eae76b"
+}),
+  databaseURL: "https://envo-377917-default-rtdb.europe-west1.firebasedatabase.app"
+});
+const db = admin.firestore();
+exports.storeFormDetails = (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let subject = req.body.subject;
+    let message = req.body.message;
+    db.collection("formDetails").add({
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
+        })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding doc: ", error);
+        });
+}
